8bf29c0b8023ac451b21481111d4bb6c389090f1
Kanishka Prajapat <kanishkaprajapatwork@gmail.com>
2025-09-25 09:15:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public ArrayList<String> generateBinary(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            result.add(Integer.toBinaryString(i));
+        }
+        return result;
+    }
+}

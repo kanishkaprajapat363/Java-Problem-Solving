391e7afa323e8390ae95bef1a1a10c1d3caab479
Kanishka Prajapat <kanishkaprajapatwork@gmail.com>
2025-09-29 16:48:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bit Difference/bit-difference.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public static int countBitsFlip(int a, int b) {
+        int xor = a ^ b;
+        int count = 0;
+        
+        while (xor > 0) {
+            if ((xor & 1) != 0) {
+                count++;
+            }
+            xor = xor >> 1;
+        }
+        
+        return count;
+    }
+}

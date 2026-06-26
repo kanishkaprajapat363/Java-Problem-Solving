c271efc88921932e358ae2edb5abb5e259fc1357
Kanishka Prajapat <kanishkaprajapatwork@gmail.com>
2026-06-26 15:25:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum Except First and Last/sum-except-first-and-last.java
@@ -0,0 +1,13 @@
+class Solution {
+    int sumExceptFirstLast(int[] arr) {
+        // your code here
+        int sum=0;
+        
+        for(int i=1;i<arr.length-1;i++){
+            sum+=arr[i];
+        }
+        
+        return sum;
+        
+    }
+}

7d0f7d946da5e00dcc8b9a9bb182cd78dc7f056e
Kanishka Prajapat <kanishkaprajapatwork@gmail.com>
2026-06-26 15:26:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMin(int n) {
+        int[] coins = {10, 5, 2, 1};
+        int count = 0;
+
+        for (int coin : coins) {
+            count += n / coin;
+            n %= coin;
+        }
+
+        return count;
+    }
+}

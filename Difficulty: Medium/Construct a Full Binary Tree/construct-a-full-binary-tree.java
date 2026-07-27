edd448adada4c55b09604bf3945f76a719148b9b
Kanishka Prajapat <kanishkaprajapatwork@gmail.com>
2026-07-27 19:46:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Construct a Full Binary Tree/construct-a-full-binary-tree.java
@@ -0,0 +1,45 @@
+/* Structure of Binary Tree Node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+} */
+
+class Solution {
+
+    int preIndex;
+    HashMap<Integer, Integer> map;
+
+    public Node constructBinaryTree(int[] pre, int[] preMirror) {
+        preIndex = 0;
+        map = new HashMap<>();
+
+        for (int i = 0; i < preMirror.length; i++) {
+            map.put(preMirror[i], i);
+        }
+
+        return build(pre, preMirror, 0, preMirror.length - 1);
+    }
+
+    private Node build(int[] pre, int[] preMirror, int l, int h) {
+        if (preIndex >= pre.length || l > h)
+            return null;
+
+        Node root = new Node(pre[preIndex++]);
+
+        // Leaf node
+        if (l == h || preIndex >= pre.length)
+            return root;
+
+        int idx = map.get(pre[preIndex]);
+
+        root.left = build(pre, preMirror, idx, h);
+        root.right = build(pre, preMirror, l + 1, idx - 1);
+
+        return root;
+    }
+}
